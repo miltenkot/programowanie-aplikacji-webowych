9c27fc71c15c0f6d37b2cbe78cec606758d88317
Bartłomiej Lańczyk <miltenkot@gmail.com>
2021-08-21 10:15:59 +0200

    Add record elements.

--- konw2/sounds/src/index.ts
+++ konw2/sounds/src/index.ts
@@ -1,79 +1,131 @@
-let rideSound: HTMLAudioElement;
-let kickSound: HTMLAudioElement;
-let boomSound: HTMLAudioElement;
-let hihatSound: HTMLAudioElement;
-let openhat: HTMLAudioElement;
-let snareSound: HTMLAudioElement;
-let tomSound: HTMLAudioElement;
-let tinkSound: HTMLAudioElement;
-let clapSound: HTMLAudioElement;
-
-appStart();
-
-var sound = {
-
-    'q': function () {
-        play(rideSound)
-    },
-    'w': function () {
-        play(kickSound)
-    },
-    'e': function () {
-        play(boomSound)
-    },
-    'r': function () {
-        play(hihatSound)
-    },
-    't': function () {
-        play(openhat)
-    },
-    'y': function () {
-        play(snareSound)
-    },
-    'u': function () {
-        play(tomSound)
-    },
-    'i': function () {
-        play(tinkSound)
-    },
-    'o': function () {
-        play(clapSound)
-    }
-};
-
-function play(audio: HTMLAudioElement): void {
-    audio.currentTime = 0
-    audio.play()
+const soundElements: HTMLAudioElement[] = 
+[document.querySelector('[data-sound="boom"]'), 
+document.querySelector('[data-sound="clap"]'),
+document.querySelector('[data-sound="hihat"]'),
+document.querySelector('[data-sound="kick"]'),
+document.querySelector('[data-sound="openhat"]'),
+document.querySelector('[data-sound="ride"]'),
+document.querySelector('[data-sound="snare"]'),
+document.querySelector('[data-sound="tink"]') 
+];
+enum Sounds {
+    boom = 0,
+    clap = 1,
+    hithat = 2,
+    kick = 3,
+    openhat = 4,
+    ride = 5,
+    snare = 6,
+    tink = 7
 }
+const playButtons: HTMLButtonElement[] = [
+    document.querySelector('#track1Play'),
+    document.querySelector('#track2Play'),
+    document.querySelector('#track3Play'),
+    document.querySelector('#track4Play')
+  ];
+const recordButtons: HTMLButtonElement[] = [
+      document.querySelector('#track1Record'),
+      document.querySelector('#track2Record'),
+      document.querySelector('#track3Record'),
+      document.querySelector('#track4Record')
+    ];
+const checkBoxes: HTMLInputElement[] = [
+  document.querySelector('#track1Checkbox'),
+  document.querySelector('#track2Checkbox'),
+  document.querySelector('#track3Checkbox'),
+  document.querySelector('#track4Checkbox')
+];
+const PlaySelectedButton: HTMLInputElement = document.querySelector('#PlaySelectedButton');
+const chanels: {key,time}[][] = [];
+chanels[0]=[];
+chanels[1]=[];
+chanels[2]=[];
+chanels[3]=[]
+const startRecordTimes: number[] = [];
 
-function appStart(): void {
-    document.body.addEventListener('keypress', onKeyDown);
-    getSounds();
+document.body.addEventListener('keypress', handleKeyPress);
+PlaySelectedButton.addEventListener('click', playSelectedTracks);
 
-}
+recordButtons.forEach((button, i: number) => button.addEventListener('click', (ev) => {
+    chanels[i] = [];
+    startRecordTimes[i] = ev.timeStamp;
+}))
 
-function getSounds(): void {
-    rideSound = document.querySelector('[data-sound="ride"]');
-    kickSound = document.querySelector('[data-sound="kick"]');
-    boomSound = document.querySelector('[data-sound="boom"]');
-    hihatSound = document.querySelector('[data-sound="hihat"]');
-    openhat = document.querySelector('[data-sound="openhat"]');
-    snareSound = document.querySelector('[data-sound="snare"]');
-    tomSound = document.querySelector('[data-sound="tom"]');
-    tinkSound = document.querySelector('[data-sound="tink"]');
-    clapSound = document.querySelector('[data-sound="clap"]');
-    
-}
+playButtons.forEach((button, i: number) => button.addEventListener('click', () => {
+        chanels[i].forEach(sound =>{
+        const timeout = sound.time - startRecordTimes[i];
+        setTimeout(() => playSound(sound.key),timeout);
+    })
+}))
 
-function onKeyDown(ev: KeyboardEvent): void {
-    console.log(ev);
+function handleKeyPress(ev: KeyboardEvent): void {
     const key = ev.key;
-    const timeStamp = ev.timeStamp;
-    playSoundFor(key);
+    const time = ev.timeStamp;
 
+    checkBoxes.forEach((checkbox, i) => {
+        if(checkBoxes[i].checked) {
+            chanels[i].push({
+            key,
+            time
+            })
+        }
+    })
+    playSound(key);
+}   
+
+function playSound(key: string){
+
+    switch(key){
+        case 'q':
+            soundElements[Sounds.boom].currentTime = 0;
+            soundElements[Sounds.boom].play()
+        break;
+
+        case 'w':
+            soundElements[Sounds.clap].currentTime = 0;
+            soundElements[Sounds.clap].play()
+        break;
+
+        case 'e':
+            soundElements[Sounds.hithat].currentTime = 0;
+            soundElements[Sounds.hithat].play()
+        break;
+
+        case 'r':
+            soundElements[Sounds.kick].currentTime = 0;
+            soundElements[Sounds.kick].play()
+        break;
+        ////////////////////////////////
+        case 'a':
+            soundElements[Sounds.openhat].currentTime = 0;
+            soundElements[Sounds.openhat].play()
+        break;
+
+        case 's':
+            soundElements[Sounds.ride].currentTime = 0;
+            soundElements[Sounds.ride].play()
+        break;
+
+        case 'd':
+            soundElements[Sounds.snare].currentTime = 0;
+            soundElements[Sounds.snare].play()
+        break;
+
+        case 'f':
+            soundElements[Sounds.tink].currentTime = 0;
+            soundElements[Sounds.tink].play()
+        break;
+    }
 }
 
-function playSoundFor(key: string): void {
-    sound[key]();
+function playSelectedTracks() {
+    checkBoxes.forEach((box, i:number) => {
+        if(checkBoxes[i].checked){
+            chanels[i].forEach(sound =>{
+                const timeout = sound.time - startRecordTimes[i];
+                setTimeout(() => playSound(sound.key),timeout);
+            })
+        }
+    });
 }
-
